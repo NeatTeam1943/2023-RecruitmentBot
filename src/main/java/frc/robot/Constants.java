45d0aa09147d72299c90fff01f15a5df6a45d8ed
LiranBratt2121 <brattlirannin@gmail.com>
2023-08-27 23:01:51 +0300

    Constant: Adjust `Constants`

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,10 +6,10 @@ public final class Constants {
   }
 
   public static class DriveTrainConstants {
-    public static final int kLeftFrontPort = 1;
-    public static final int kLeftRearPort = 2;
-    public static final int kRightFrontPort = 3;
-    public static final int kRightRearPort = 4;
+    public static final int kLeftFrontPort = 4;
+    public static final int kLeftRearPort = 3;
+    public static final int kRightFrontPort = 7;
+    public static final int kRightRearPort = 8;
   }
 
   public static class StorageConstants {
@@ -17,22 +17,23 @@ public final class Constants {
     public static final int kTopSolonoidRetract = 1;
 
     public static final int kBottomSolonoidDeploy = 4;
-    public static final int kBottomSolonoidRetract = 3;
+    public static final int kBottomSolonoidRetract = 5;
 
   }
 
   public static class ShooterCostants {
-    public static final int kLeftFlyWheel = 0;
-    public static final int kRightFlyWheel = 1;
+    public static final int kLeftFlyWheel = 2;
+    public static final int kRightFlyWheel = 5;
 
     public static final int kEncoderDistancePerPulse = 1;
 
     public static final int kLeftEncoderChannelA = 0;
     public static final int kLeftEncoderChannelB = 1;
 
-    public static final int kRightEncoderChannelA = 0;
-    public static final int kRightEncoderChannelB = 1;
+    public static final int kRightEncoderChannelA = 3;
+    public static final int kRightEncoderChannelB = 2;
 
     public static final double kShoot = 15000; 
+
   }
 }
