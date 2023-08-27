bb8325557dd52021e6322ba46bd56a2261544bc7
LiranBratt2121 <brattlirannin@gmail.com>
2023-08-27 23:02:14 +0300

    Robot: Add compressor initialization

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -19,6 +21,9 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+
+  private Compressor m_compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -28,6 +33,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    m_compressor.enableDigital();
   }
 
   /**
