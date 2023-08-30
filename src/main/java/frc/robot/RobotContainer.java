3abda1b2f69b64ed9878b5e17e9e56154dc19be3
ErezAmirSolomon <salmonerez@gmail.com>
2023-08-30 20:53:26 +0300

    It Workks!!!!!!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,18 +1,17 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Shoot;
+// import frc.robot.commands.Shoot;
 import frc.robot.commands.TimedShoot;
 import frc.robot.subsystems.Chassis;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Storage;
 import frc.robot.subsystems.Storage.WhichStorage;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -25,46 +24,44 @@ public class RobotContainer {
     private final Storage m_storage = new Storage();
     private final Shooter m_shooter = new Shooter();
 
-//     private final Shoot m_shoot = new Shoot(m_shooter);
     private final TimedShoot m_timedShoot = new TimedShoot(m_shooter); 
 
-    private final Dictionary<String, Command> shootingCommands = new Hashtable<>();
+    // private final CommandBase m_toggleTopSolonid = Commands.run(() -> m_storage.getTopSolonoid().toggle(), m_storage);
+    // private final CommandBase m_toggleBottomSolonid = Commands.run(() -> m_storage.getBottomSolonoid().toggle(), m_storage);
+
+    // private final CommandBase m_retrackeBaseSolonoid  = Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kReverse), m_storage);
+    // private final CommandBase m_wait1Sec = new WaitCommand(1);
+    // private final CommandBase m_deployBaseSolonoid = Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kForward), m_storage);
+    // private final CommandBase m_retrackeTopSolonoid = Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kReverse), m_storage);
+    // private final CommandBase m_deployTopsolonoid = Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kForward), m_storage);
 
     public RobotContainer() {
         configureBindings();
 
         m_drive.setDefaultCommand(Commands.run(() -> m_drive.drive(m_driverController),  m_drive));
+        m_shooter.setDefaultCommand(Commands.run(() -> m_shooter.setFlyWheelSpeed(m_driverController.getRightTriggerAxis()), m_shooter));
     }
 
     private void configureBindings() {
-        // shootingCommands.put("Get to the right rpm", m_shoot);
-        shootingCommands.put("Move motors for 5s", m_timedShoot);
-        shootingCommands.put("Retracke base Solonoid",
-                Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kReverse), m_storage));
-        shootingCommands.put("wait 1s", new WaitCommand(1));
-        shootingCommands.put("Deploy base solonoid",
-                Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kForward), m_storage));
-        shootingCommands.put("Retracke top solonoid",
-                Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kReverse), m_storage));
-        shootingCommands.put("Deploy top solonoid",
-                Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kForward), m_storage));
-        shootingCommands.put("Toggle top solonoid",
-                Commands.run(() -> m_storage.getTopSolonoid().toggle(), m_storage));
-        shootingCommands.put("Toggle base solonoid",
-                Commands.run(() -> m_storage.getBottomSolonoid().toggle(), m_storage));
+        m_driverController.leftBumper().whileTrue(Commands.sequence(
+          m_timedShoot,
+          Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kReverse), m_storage),
+          new WaitCommand(1),
+          Commands.run(() -> m_storage.setSolonid(WhichStorage.BOTTOM, Value.kForward), m_storage),
+          Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kReverse), m_storage),
+          new WaitCommand(1),
+          Commands.run(() -> m_storage.setSolonid(WhichStorage.TOP, Value.kForward), m_storage)));
 
-        m_driverController.x().whileTrue(Commands.sequence(
-        //   shootingCommands.get("Get to the right rpm"),
-          shootingCommands.get("Move motors for 5s"),
-          shootingCommands.get("Retracke base Solonoid"),
-          shootingCommands.get("wait 1s"),
-          shootingCommands.get("Deploy base solonoid"),
-          shootingCommands.get("Retracke top solonoid"),
-          shootingCommands.get("wait 1s"),
-          shootingCommands.get("Deploy top solonoid")));
 
-        m_driverController.y().toggleOnTrue(shootingCommands.get("Toggle top solonoid"));
-        m_driverController.a().toggleOnTrue(shootingCommands.get("Toggle base solonoid"));
+        m_driverController.x().onTrue(Commands.run(() -> m_storage.getTopSolonoid().set(Value.kForward), m_storage));
+        m_driverController.y().onTrue(Commands.run(() -> m_storage.getTopSolonoid().set(Value.kReverse), m_storage));
+
+        m_driverController.a().onTrue(Commands.run(() -> m_storage.getBottomSolonoid().set(Value.kForward), m_storage));
+        m_driverController.b().onTrue(Commands.run(() -> m_storage.getBottomSolonoid().set(Value.kReverse), m_storage));
+    }
+
+    public Storage getStorage(){
+        return m_storage;
     }
 
     public Command getAutonomousCommand() {
